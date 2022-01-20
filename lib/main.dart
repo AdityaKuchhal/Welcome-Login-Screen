bb539848de940d9916a8c075b28839921a6b24d1
AdityaKuchhal <adikuchhal@gmail.com>
2022-01-20 15:38:25 +0530

    Starter

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Plantify());
+}
+
+class Plantify extends StatelessWidget {
+  const Plantify({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
