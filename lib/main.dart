abcf6caff45844331cfca8772fec46dc216bafe0
AdityaKuchhal <adikuchhal@gmail.com>
2022-01-26 19:30:31 +0530

    Starter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:loginscreen/login_screen.dart';
-import 'package:loginscreen/signup_screen.dart';
-import 'package:loginscreen/welcome_screen.dart';
 
 void main() {
   runApp(const FitnessApp());
@@ -13,20 +9,6 @@ class FitnessApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.black,
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ));
-    return MaterialApp(
-      initialRoute: WelcomeScreen.id,
-      routes: {
-        WelcomeScreen.id: (context) => const WelcomeScreen(),
-        LoginScreen.id: (context) => const LoginScreen(),
-        SignupScreen.id: (context) => const SignupScreen(),
-      },
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp();
   }
 }
